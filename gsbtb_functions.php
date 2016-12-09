94cdadff7d48d11aa2b1655de3b5063b322bb586
corgijames <lewis.james+corgibot@gmail.com>
2016-12-09 18:47:32 +0200

    skeleton api

--- gsbtb_functions.php
+++ gsbtb_functions.php
@@ -6,4 +6,8 @@ Functions to include from a Wordpress functions.php file that will provide inter
 
 */
 
+require_once('gsbtb_tool_clientv0.1.php');
+
+
+
 ?>
--- gsbtb_tool_clientv0.1.php
+++ gsbtb_tool_clientv0.1.php
@@ -5,4 +5,26 @@ PHP platform agnostic client layer to the GSBTB API
 
 */
 
+function gsbtbApiGetPersons($pageStart, $pageLength) {
+	return json_decode("""{
+	[
+		"guidPerson1" : [ "name" : "First Last", "bio" : "biography text", "skills" : [ "skill1", "skill2", "skill3" ] ],
+		"guidPerson2" : [ "name" : "First2 Last2", "bio" : "biography text2", "skills" : [ "skill4", "skill5", "skill6" ] ]
+	]
+
+}""");
+
+}
+
+function gsbtbApiGetPerson($personId) {
+        return json_decode("""{
+
+        [ "name" : "First Last", "bio" : "biography text", "skills" : [ "skill1", "skill2", "skill3" ] ]
+
+}""");
+
+}
+
+
+
 ?>

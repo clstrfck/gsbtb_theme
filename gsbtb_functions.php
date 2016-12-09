e6f0dc410b9263c69485dc633c10a55f9c50c446
corgijames <lewis.james+corgibot@gmail.com>
2016-12-09 18:53:56 +0200

    wiring

--- gsbtb_functions.php
+++ gsbtb_functions.php
@@ -8,6 +8,13 @@ Functions to include from a Wordpress functions.php file that will provide inter
 
 require_once('gsbtb_tool_clientv0.1.php');
 
+function gsbtb_getPersons($pageStart, $pageLength) {
+	return gsbtbApiGetPersons($pageStart, $pageLength);
+}
+
+function gsbtb_getPerson($personId) {
+	return gsbtbApiGetPerson($personId);
+}
 
 
 ?>
